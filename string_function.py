b6acfa109953944b648d2b93156599bde68b6887
Nill <nillmishra09@gmail.com>
2023-07-27 00:44:08 +0530

    string using function

---
+++ string_function.py
@@ -0,0 +1,11 @@
+story = "once upon a time there was a coder named nill who used to code in github"
+#string function
+print(len(story))
+print(story.endswith("sdqhdf"))
+print(story.count("a"))
+print(story.count("wa"))
+print(story.capitalize())#dirst word ko capitalize kar k deta hai
+print(story.find("nill"))
+print(story.find("nili"))
+print(story.find("once"))
+print(story.replace("nill", "Nill Mishra"))
