3a60e837946730bf031755eeab20dd7e352f3376
Nill <nillmishra09@gmail.com>
2023-07-26 22:30:24 +0530

    using of operator  and types

--- operators.py
+++ operators.py
@@ -17,4 +17,4 @@ bool1 =True
 bool2 = False
 print("The value of bool1 and bool2 is, bool1",bool1 and bool2)
 print("The value of bool1 and bool2 is, bool1",bool1 or bool2)
-print("The value of bool1 and bool2 is, bool1",not bool2)
+print("The value of bool1 and bool2 is, bool1",(not bool2))
