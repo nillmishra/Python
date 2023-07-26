dfa6cb014c0076ca40dd1e0252bdb66d50aeec48
Nill <nillmishra09@gmail.com>
2023-07-26 22:31:15 +0530

    variable and type of  data type

---
+++ variable.py
@@ -0,0 +1,28 @@
+from sre_parse import TYPE_FLAGS
+
+
+a = 'Nill Mishra'
+f = "Hello  World" 
+g = '''Hello 
+Myself 
+Nill 
+Mishra'''
+b = 5
+c = 4.56
+d = False
+h = None
+#printing the variable
+print(a)
+print(type(a))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(h)
+print(type(h))
