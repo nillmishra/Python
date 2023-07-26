ad1c53c5aaf53f99f785b22711d1505c602b5a72
Nill <nillmishra09@gmail.com>
2023-07-26 22:29:29 +0530

    suing o  foperator in python

---
+++ operators.py
@@ -0,0 +1,20 @@
+#arthmetic operator
+print('The value of a + b is',3+5)
+a =34
+#assignment operator
+print("the value of a is ", a)
+a += 5
+print("the value of a is ", a)
+#comparision operator
+b = (7>4)
+print(b)
+c =6<3
+print(c)
+v = (6==7)
+print(v)
+#logical operator 
+bool1 =True
+bool2 = False
+print("The value of bool1 and bool2 is, bool1",bool1 and bool2)
+print("The value of bool1 and bool2 is, bool1",bool1 or bool2)
+print("The value of bool1 and bool2 is, bool1",not bool2)
