f2a886dc2a88ccae71ea0877823e8faf8ecac265
Nill <nillmishra09@gmail.com>
2023-07-28 00:30:39 +0530

    class work

---
+++ class work/new.py
@@ -0,0 +1,13 @@
+mystr = "university"
+my_list_enumerate = list(enumerate(mystr))
+print('my_list_enumerate=', my_list_enumerate)
+print('len(mystr)=',len(mystr))
+print('''Tell me "What's your name?"''') 
+print('Tell me "What\'s your name?"') 
+print("Tell me 'What\'s your name?'") 
+print("Tell me \"What's your name?\"") 
+
+print("c:\\user\\bin\\mytext.txt")
+print("This line is having a \n character")
+print("This line  is having a \t character")
+print("ABC written in \x41 \x42 \x43 (Hex) representation")
